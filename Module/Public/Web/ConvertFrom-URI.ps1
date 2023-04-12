aa9706da177e36e03db36682f1ea76bc42b51995
Steve Brown <steve.brown+github@brownserve.co.uk>
2023-04-13 00:52:10 +0100

    Allow multiple objects

--- Module/Public/Web/ConvertFrom-URI.ps1
+++ Module/Public/Web/ConvertFrom-URI.ps1
@@ -9,7 +9,7 @@ function ConvertFrom-URI
             Position = 0, 
             ValueFromPipeline = $true, 
             ValueFromPipelineByPropertyName = $true)]
-        [string]
+        [string[]]
         $InputObject,
 
         # Returns the object as a Hashtable instead of a custom object
@@ -25,38 +25,40 @@ function ConvertFrom-URI
             I've been quite loose with the protocol so we can convert things like FTP/SMTP etc.
         #>
         $RegEx = '(?:(?<Protocol>\w*):\/\/)?(?:(?<Subdomain>[\w\.]+)?\.)?(?<Hostname>\w+)\.(?<Domain>\w+)\:?(?<Port>\d+)?(?:\/)?(?<Path>.*)?'
-        $Return = $null
+        $Return = @()
     }
     
     process
     {
-        if ($InputObject -match $RegEx)
-        {
-            $Hashtable = $Matches
-            # Remove the key '0' which includes the entire regex match, and instead create a key called URI
-            $Hashtable.Add('URI', $InputObject)
-            $Hashtable.Remove(0)
-            # Somehow we're getting an empty string returned from the path regex, not sure how but for now just filter it out
-            if ('' -eq $Hashtable.Path)
+        $InputObject | ForEach-Object {
+            if ($_ -match $RegEx)
             {
-                $Hashtable.Remove('Path')
-            }
+                $Hashtable = $Matches
+                # Remove the key '0' which includes the entire regex match, and instead create a key called URI
+                $Hashtable.Add('URI', $InputObject)
+                $Hashtable.Remove(0)
+                # Somehow we're getting an empty string returned from the path regex, not sure how but for now just filter it out
+                if ('' -eq $Hashtable.Path)
+                {
+                    $Hashtable.Remove('Path')
+                }
 
-            if ($AsHashtable)
-            {
-                $Return = $AsHashtable
-            }
-            else
-            {
-                # Cast to a custom object
-                $Return = [pscustomobject]$Hashtable
+                if ($AsHashtable)
+                {
+                    $Return += $AsHashtable
+                }
+                else
+                {
+                    # Cast to a custom object
+                    $Return += [pscustomobject]$Hashtable
+                }
             }
         }
     }
     
     end
     {
-        if ($null -ne $Return)
+        if ($Return -ne @())
         {
             return $Return
         }
