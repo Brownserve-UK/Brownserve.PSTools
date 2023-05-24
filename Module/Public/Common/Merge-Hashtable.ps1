5f77dc9cbdcdc4438f3eaa4f935b99b863639638
Steve Brown <steve.brown+github@brownserve.co.uk>
2023-05-24 15:31:42 +0100

    Tidy up messaging a bit

--- Module/Public/Common/Merge-Hashtable.ps1
+++ Module/Public/Common/Merge-Hashtable.ps1
@@ -40,22 +40,21 @@ function Merge-Hashtable
                     if ($Return.Keys -contains $InputObjectKeyName)
                     {
                         $BaseObjectValue = $Return.($InputObjectKeyName)
-                        Write-Debug "BaseObject already contains key of '$($InputObjectKeyName)'"
+                        Write-Verbose "BaseObject already contains key of '$($InputObjectKeyName)'"
                         # If the key does already exist and we're doing a deep merge we need to see what content
                         # we are working with
                         if ($Deep)
                         {
                             if ($BaseObjectValue -is [array])
                             {
-                                Write-Debug "Key '$InputObjectKeyName' is an array"
                                 # Validate that the value we're bringing in is also an array
                                 if ($InputObjectValue -is [array])
                                 {
                                     # We make the concious choice to remove duplicate entries when merging arrays, I'm not sure
                                     # if this is standard behaviour when merging objects but we can could make this a parameter if we need to
-                                    Write-Debug "Merging array: $InputObjectKeyName with values:$($BaseObjectValue -join "`n")`n$($InputObjectValue -join "`n")"
+                                    Write-Verbose "Key '$InputObjectKeyName' is an array, performing merge."
+                                    Write-Debug "BaseObject values:`n$($BaseObjectValue -join "`n")`nInputObject values:`n$($InputObjectValue -join "`n")"
                                     $MergedArray = $BaseObjectValue + $InputObjectValue
-                                    Write-Debug "$MergedArray"
                                     $Return.($InputObjectKeyName) = ($MergedArray | Select-Object -Unique)
                                 }
                                 else
@@ -67,7 +66,7 @@ function Merge-Hashtable
                             {
                                 if ($InputObjectValue -is [hashtable])
                                 {
-                                    Write-Debug "Merging hashtable: $($InputObjectKeyName)"
+                                    Write-Verbose "Key '$InputObjectKeyName' is a hashtable, performing merge."
                                     $Return.($InputObjectKeyName) = Merge-Hashtable `
                                         -BaseObject $Return.($InputObjectKeyName) `
                                         -InputObject $InputObjectValue `
