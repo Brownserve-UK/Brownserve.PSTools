5d17dc301b14856f6cff2039594d7dcbd47ce4bd
Steve Brown <steve.brown+github@brownserve.co.uk>
2023-08-25 19:22:04 +0000

    feat: cmdlet now working

--- Module/Public/Build/Read-BrownserveChangelog.ps1
+++ Module/Public/Build/Read-BrownserveChangelog.ps1
@@ -30,14 +30,14 @@ function Read-BrownserveChangelog
         [string]
         $VersionPattern = '^#*\s\[v(?<version>[0-9]+\.[0-9]+\.[0-9]+)\]\((?:.*)\)\s\([0-9]+\-[0-9]+\-[0-9]+\)$',
 
-        # The regex pattern for matching the repo URL.
+        # The regex pattern for matching the release URL.
         # It should always contain a capture group named "url" and this what the regex searched will use to extract your url
         [Parameter(
             Mandatory = $false,
             DontShow
         )]
         [string]
-        $RepoURLPattern = '(?<url>http(?:.*))\/tree',
+        $ReleaseURLPattern = '(?<url>[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?)',
 
         # The regex pattern for matching the date of the last release.
         # It should always contain a capture group named "date" and this what the regex searched will use to extract your date
@@ -71,6 +71,11 @@ function Read-BrownserveChangelog
         $LineCount = 0
 
         <#
+            As we go through each line we'll begin building a version history
+        #>
+        $VersionHistory = @()
+
+        <#
             To get the changes that feature in the latest entry (i.e. release notes) we need to know both the current and
             previous released versions.
             We can then find the text block between these lines which should be our release notes.
@@ -89,63 +94,85 @@ function Read-BrownserveChangelog
         # Go through each line until we find what we need
         $Changelog | ForEach-Object {
             $Line = $_.Trim()
-            # Return the URL of the repo if we can find it
-            if (-not $RepoUrl)
+            # See if the line matches a version
+            $VersionMatch = [regex]::Match($Line, $VersionPattern)
+            if ($VersionMatch.Success)
             {
-                $RepoUrlMatch = [regex]::Match($Line, $RepoUrlPattern)
-                if ($RepoUrlMatch.Success)
+                Write-Debug "Current line is: $LineCount"
+                Write-Debug "Line contents: $Line"
+                # Congratulations this line matches a version number. 🎉 we'll store it for use later
+                $ThisVersion = [semver]$VersionMatch.Groups['version'].Value
+                # This line should also contain a date of the release, we'll want that too
+                $ReleaseDateMatch = [regex]::Match($Line, $ReleaseDatePattern)
+                if ($ReleaseDateMatch.Success)
                 {
-                    $RepoUrl = $RepoUrlMatch.Groups['url'].Value
-                    Write-Verbose "Repo URL determined to be $RepoURL"
+                    $ThisReleaseDate = $ReleaseDateMatch.Groups['date'].Value
                 }
-            }
-            # If we don't already have our current version then see if this line contains it...
-            if (-not $CurrentVersion)
-            {
-                $RegexMatch = [regex]::Match($Line, $VersionPattern)
-                if ($RegexMatch.Success)
+                $ReleaseURLMatch = [regex]::Match($Line, $ReleaseURLPattern)
+                if ($ReleaseURLMatch.Success)
                 {
-                    # This line matches our version regex!
-                    # This has to be the first line we've come across that matches it so it must be the current version.
-                    # Extract the version number from our capture group
-                    $CurrentVersion = [version] $RegexMatch.Groups['version'].Value
-                    # The current release notes will start on the _next_ line after the version number 
+                    $ThisURL = $ReleaseURLMatch.Groups['url'].Value
+                }
+                <#
+                    If we don't already have the current version then this must be it as our changelog is descending order
+                    Set the $CurrentVersionVariable, we'll also be able to tell where the current release notes start as
+                    they will be on the next line _after_ the version line.
+                    Similarly the line for inserting a new entry will be the line _before_ the current versions line
+                #>
+                if (-not $CurrentVersion)
+                {
+                    $CurrentVersion = $ThisVersion
+                    # The current release notes will start on the _next_ line after the version number
                     $ReleaseNotesStartOn = $LineCount + 1
                     # If we want to insert any new changelog entries then we'll need to known where we can do that
                     # It will be the line _before_ our line with the version number on
                     $NewChangelogLine = $LineCount - 1
                     Write-Verbose "Current version determined to be $CurrentVersion"
                     # Now we've got our current version we can also extract the date of the last release
-                    $LastReleaseDateMatch = [regex]::Match($Line, $ReleaseDatePattern)
-                    if ($LastReleaseDateMatch.Success)
+                    $LastReleaseDate = $ThisReleaseDate
+                    Write-Verbose "Last release date determined to be $LastReleaseDate"
+                }
+                else
+                {
+                    <#
+                        We should only end up here once $CurrentVersion has been set on a previous line read.
+                        This should ensure we can never accidentally read the CurrentVersion as the PreviousVersion
+                    #>
+                    if (-not $PreviousVersion)
                     {
-                        $LastReleaseDate = Get-Date $LastReleaseDateMatch.Groups['date'].Value -ErrorAction 'Stop'
-                        Write-Verbose "Last release date determined to be $LastReleaseDate"
+                        # We've found a potential match!
+                        $PreviousVersion = $ThisVersion
+                        # The release notes will end on the line _before_ the previous version number
+                        $ReleaseNotesEndOn = $LineCount - 1
+                        <#
+                            Just in case we've somehow ended up with PreviousVersion and CurrentVersion being the same
+                            (i.e. we've read the same line twice!)
+                            Then we reset the variables and carry on
+                        #>
+                        if ($PreviousVersion -eq $CurrentVersion)
+                        {
+                            $PreviousVersion = $null
+                            $ReleaseNotesEndOn = $null
+                        }
                     }
                 }
-            }
-            # If we've found our current version number then from here on out the rest of the document will be our changelog, start capturing it!
-            if ($CurrentVersion)
-            {
-                $ChangelogText += $Line
-            }
-            # If we don't already have the previous version number see if this line contains it...
-            if (-not $PreviousVersion)
-            {
-                $RegexMatch = [regex]::Match($Line, $VersionPattern)
-                if ($RegexMatch.Success)
+                <#
+                    Once the $CurrentVersion variable has been set we know that we've gone past the header section and
+                    whitespace at the top of the changelog and we should be good to start storing every line of text to
+                    return the changelog sans the header.
+                #>
+                if ($CurrentVersion)
                 {
-                    # We've found a potential match!
-                    $PreviousVersion = [version] $RegexMatch.Groups['version'].Value
-                    # The release notes will end on the line _before_ the previous version number
-                    $ReleaseNotesEndOn = $LineCount - 1
-                    # If we've hit the current version then we haven't gone far enough back!
-                    # Clear our variables and continue looking
-                    if ($PreviousVersion -eq $CurrentVersion)
-                    {
-                        $PreviousVersion = $null
-                        $ReleaseNotesEndOn = $null
-                    }
+                    $ChangelogText += $Line
+                }
+
+                <#
+                    Create an object of the data we've gathered
+                #>
+                $VersionHistory += [pscustomobject]@{
+                    Version     = $ThisVersion
+                    ReleaseDate = $ThisReleaseDate
+                    URL         = $ThisURL
                 }
             }
             # Finally increase the line count for the next loop
@@ -167,6 +194,21 @@ function Read-BrownserveChangelog
         try
         {
             $ReleaseNotes = $Changelog[$ReleaseNotesStartOn..$ReleaseNotesEndOn]
+
+            # Try to trim off any empty lines at the start and end of the release note text
+            $LastLine = $ReleaseNotes.Count
+            while (!$ReleaseNotes[-1])
+            {
+                $LastLine = $LastLine -1
+                $ReleaseNotes = $ReleaseNotes[0..$LastLine]
+            }
+            $FirstLine = 0
+            while (!$ReleaseNotes[0])
+            {
+                $LastLine = $ReleaseNotes.Count
+                $FirstLine ++
+                $ReleaseNotes = $ReleaseNotes[$FirstLine..$LastLine]
+            }
         }
         catch
         {
@@ -181,6 +223,16 @@ function Read-BrownserveChangelog
         if (!$ReleaseNotes)
         {
             throw 'Unable to work out current release notes.'
+        }
+
+        # TODO: Do we want to create a "LatestVersion object?"
+        $Return += [pscustomobject]@{
+            ChangeLogPath  = $ChangelogPath # The path to the changelog - useful when piping into other cmdlets
+            VersionHistory = $VersionHistory | Sort-Object -Property Version -Descending # The version history of the changelog
+            LatestVersion  = $CurrentVersion # The latest version according to the changelog
+            ReleasedOn     = $LastReleaseDate # The date of the last release
+            ReleaseNotes   = $ReleaseNotes -join [System.Environment]::NewLine # The release notes for the latest version only
+            NextEntryLine  = $NewChangelogLine # This will be the line that we can start inserting new entries into
         }
     }
     end
