6ee2204d826a76983e4af3c92e5289bcc69658a2
Steve Brown <steve.brown+github@brownserve.co.uk>
2024-04-19 20:18:08 +0100

    refactor: rework the Update-BSPowerShellModule cmdlet
    Uses new logic

--- Module/Public/PowerShell/Update-BrownservePowerShellModule.ps1
+++ Module/Public/PowerShell/Update-BrownservePowerShellModule.ps1
@@ -4,110 +4,118 @@ function Update-BrownservePowerShellModule
     param
     (
         # The path where the PowerShell module should be created
-        [Parameter(Mandatory = $true)]
+        [Parameter(
+            Mandatory = $true,
+            Position = 0
+        )]
         [string]
         $Path,
-
-        # An optional description for the module
-        [Parameter(Mandatory = $false)]
-        [string]
-        $Description,
-
-        # Any custom code to include when creating the module
-        [Parameter(Mandatory = $false)]
-        [Alias('Customizations')]
-        [string]
-        $Customisations,
 
         # Forces overwriting of files
         [Parameter(Mandatory = $false)]
         [switch]
         $Force
     )
-    
     begin
     {
-        # First extract any custom content from the module so it can be preserved
+    }
+    process
+    {
+        if ($Path -notmatch '\.psm1$')
+        {
+            $Path = $Path + '.psm1'
+        }
+        Resolve-Path $Path -ErrorAction 'Stop' | Out-Null
+        # Get the current module content
+        $ModuleContent = Get-BrownserveContent -Path $Path -ErrorAction 'Stop'
+
+        $ModuleParams = @{}
         try
         {
-            $CurrentModuleContent = Read-BrownservePowerShellModule -ModulePath $Path -ErrorAction 'Stop'
+            $CustomisationSteps = $ModuleContent | Select-BrownserveContent `
+                -After '### Start user defined module steps' `
+                -Before '### End user defined module steps' `
+                -FailIfNotFound `
+                -ErrorAction 'Stop'
         }
         catch
         {
-            throw "Failed to interpolate module at '$Path' are you sure it's a Brownserve PowerShell module?`n$($_.Exception.Message)"
-        }
-    }
-    
-    process
-    {
-        if ($CurrentModuleContent.Description)
-        {
-            if ($Description)
+            if (!$Force)
             {
-                if ($Force)
-                {
-                    $ModuleDescription = $Description
-                }
-                else
-                {
-                    throw "Module at '$Path' already has a description, to overwrite please use the -Force parameter."
-                }
-            }
-            else
-            {
-                $ModuleDescription = ($CurrentModuleContent.Description | Out-String).Trim()
+                throw "Failed to extract customisation steps from module.`n$($_.Exception.Message)"
             }
         }
 
-        if ($CurrentModuleContent.CustomCode)
+        if ($CustomisationSteps)
         {
-            if ($Customisations)
-            {
-                if ($Force)
-                {
-                    $ModuleCustomisations = $Customisations
-                }
-                else
-                {
-                    throw "Module at '$Path' already has customisations, to overwrite please use the -Force parameter."
-                }
-            }
-            else
-            {
-                $ModuleCustomisations = ($CurrentModuleContent.CustomCode | Out-String).Trim()
-            }
+            $ModuleParams.Add('Customisations', $CustomisationSteps)
+        }
+        # Check to see if we've already got a required version of PowerShell
+        if ($ModuleContent.ToString() -match '#Requires -Version ([0-9.]+)')
+        {
+            $ModuleParams.Add('RequirePowerShellVersion', $Matches[1])
+        }
+        # Check to see if Brownserve.PSTools is required
+        if ($ModuleContent.ToString() -match '#Requires -Module Brownserve.PSTools')
+        {
+            $ModuleParams.Add('RequireBrownservePSTools', $true)
+        }
+        else
+        {
+            $ModuleParams.Add('RequireBrownservePSTools', $false)
+        }
+        # Check to see if BrownserveCmdlets logic is included
+        if ($ModuleContent.ToString() -match 'BrownserveCmdlets')
+        {
+            $ModuleParams.Add('IncludeBrownserveCmdletsLogic', $true)
+        }
+        else
+        {
+            $ModuleParams.Add('IncludeBrownserveCmdletsLogic', $false)
+        }
+        # Check to see if we've already got a description - there's two potential formats we might find it in
+        # believe me - you probably don't want to mess with this regex
+        if ($ModuleContent.ToString() -match '(?m)\.DESCRIPTION\n([\s\S]+?)[\.^#]')
+        {
+            $ModuleParams.Add('Description', $Matches[1])
+        }
+        if ((!$ModuleParams.ContainsKey('Description')) -and ($ModuleContent.ToString() -match '(?m)\.DESCRIPTION\n([\s\S]+?)#'))
+        {
+            $ModuleParams.Add('Description', $Matches[1])
         }
 
-        $Params = @{}
-        if ($ModuleDescription)
-        {
-            $Params.Add('Description', $ModuleDescription)
-        }
-        if ($ModuleCustomisations)
-        {
-            $Params.Add('Customisations', $ModuleCustomisations)
-        }
         try
         {
-            $ModuleTemplate = New-BrownservePoShModuleFromTemplate @Params -ErrorAction 'Stop'
+            $ModuleTemplate = New-BrownservePoShModuleFromTemplate @ModuleParams -ErrorAction 'Stop'
+            $NewModuleContent = $ModuleTemplate | Format-BrownserveContent -ErrorAction 'Stop'
         }
         catch
         {
             throw "Failed to build module template.`n$($_.Exception.Message)"
         }
 
-        try
+        $ModuleCompare = Compare-Object `
+            -ReferenceObject $ModuleContent.Content `
+            -DifferenceObject $NewModuleContent.Content `
+            -SyncWindow 1 `
+            -ErrorAction 'Stop'
+        if ($ModuleCompare -or $Force)
         {
-            Set-Content -Path $Path -Value $ModuleTemplate -Force:$Force
+            try
+            {
+                $NewModuleContent | Set-BrownserveContent -Path $Path -ErrorAction 'Stop'
+            }
+            catch
+            {
+                throw "Failed to create new module.`n$($_.Exception.Message)"
+            }
         }
-        catch
+        else
         {
-            throw "Failed to update module content at '$Path'.`n$($_.Exception.Message)"
+            Write-Verbose 'No changes required'
         }
     }
-    
     end
     {
-        
     }
 }
