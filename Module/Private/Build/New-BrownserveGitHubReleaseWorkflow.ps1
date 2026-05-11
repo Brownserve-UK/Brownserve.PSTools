e06aaf77aecc6af3e7b69c53d06df0652d5f3698
Steve Brown <steve.brown+github@brownserve.co.uk>
2026-05-12 00:02:51 +0100

    feat: move github actions builds into templates
    This makes them more consistent with how we usually do things.

---
+++ Module/Private/Build/New-BrownserveGitHubReleaseWorkflow.ps1
@@ -0,0 +1,28 @@
+function New-BrownserveGitHubReleaseWorkflow
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ModuleName
+    )
+    begin
+    {
+        try
+        {
+            $Template = Get-Content (Join-Path $PSScriptRoot 'templates' 'psmodule_github_release.yaml.template') -Raw
+        }
+        catch
+        {
+            throw "Failed to import release workflow template.`n$($_.Exception.Message)"
+        }
+    }
+    process
+    {
+        return $Template -replace '###MODULE_NAME###', $ModuleName
+    }
+    end
+    {
+    }
+}
--- Module/Public/Build/New-BrownservePowerShellModuleBuild.ps1
+++ Module/Public/Build/New-BrownservePowerShellModuleBuild.ps1
@@ -28,7 +28,7 @@ function New-BrownservePowerShellModuleBuild
         try
         {
             Assert-Directory $RepoPath
-            $RepoPath = $RepoPath | Convert-Path # Ensure the path is recorded fully
+            $RepoPath = $RepoPath | Convert-Path
         }
         catch
         {
@@ -36,12 +36,12 @@ function New-BrownservePowerShellModuleBuild
         }
         if (!$RepoName)
         {
-            Write-Verbose "No repo name, using directory name"
+            Write-Verbose 'No repo name, using directory name'
             $RepoName = Split-Path $RepoPath -Leaf
         }
         if (!$RepoName)
         {
-            throw "Cannot determine RepoName automatically."
+            throw 'Cannot determine RepoName automatically.'
         }
     }
     process
@@ -52,132 +52,23 @@ function New-BrownservePowerShellModuleBuild
             {
                 $GitHubDirectory = Join-Path $RepoPath '.github'
                 $WorkflowDirectory = Join-Path $GitHubDirectory 'workflows'
-                $BuildWorkflowPath = Join-Path $WorkflowDirectory 'build.yaml'
+                $BuildsWorkflowPath = Join-Path $WorkflowDirectory 'builds.yaml'
+                $StageReleaseWorkflowPath = Join-Path $WorkflowDirectory 'stage-release.yaml'
                 $ReleaseWorkflowPath = Join-Path $WorkflowDirectory 'release.yaml'
-                $FilesToCheck = @( $BuildWorkflowPath, $ReleaseWorkflowPath)
 
-                $FilesToCheck | Assert-PathDoesNotExist
-
-                # These steps are always needed regardless of the build
-                $DefaultSteps = @(
-                    [ordered]@{
-                        name = 'checkout-module'
-                        uses = 'actions/checkout@v3'
-                        with = @{
-                            path = $ModuleInfo.Name
-                        }
-                    }
-                )
-
-                # This command is used to run the module build script, we build up the params separately and splat them in
-                $RunCommand = "./$($ModuleInfo.Name)/.build/build.ps1 @Params"
-
-                # Params for building/testing the module
-                $BuildTestParams = @"
-`$Params = @{
-    BranchName = `$env:GITHUB_REF
-    GitHubRepoName = '$RepoName'
-    Build = 'BuildTestAndCheck'
-    Verbose = `$true
-}
-"@
-                # Steps for building and testing the module
-                $StageReleaseParams = @"
-`$Params = @{
-    BranchName = `$env:GITHUB_REF
-    GitHubRepoName = '$RepoName'
-    Build = 'StageRelease'
-    GitHubStageReleaseToken = `$env:GitHubPAT
-    ReleaseType = '`${{inputs.release_type}}'
-    UseWorkingCopy = `$true
-    Verbose = `$true
-    }
-    if (`$env:ReleaseNotice)
-    {
-        `$Params.Add('ReleaseNotice', `$env:ReleaseNotice)
-    }
-"@
-            $ReleaseParams = @"
-`$Params = @{
-    BranchName = `$env:GITHUB_REF
-    GitHubRepoName = '$RepoName'
-    Build = 'Release'
-    GitHubReleaseToken = `$env:GitHubPAT
-    NugetFeedAPIKey = `$env:NugetFeedAPIKey
-    PSGalleryAPIKey = `$env:PSGalleryAPIKey
-    PublishTo = `${{ inputs.publish_to }}
-    Verbose = `$true
-}
-"@
-                $BuildSteps = $DefaultSteps
-                $BuildSteps += [ordered]@{
-                    name  = 'run-build-script'
-                    shell = 'pwsh'
-                    run   = $BuildTestParams + "`n" + $RunCommand
-                }
-
-                $BuildJobs = @{
-                    JobTitle = 'BuildTestAndCheck'
-                    RunsOn   = 'ubuntu-latest'
-                    Steps    = $BuildSteps
-                }
-
-                $StageReleaseSteps = $DefaultSteps
-                # We need to fetch the full history so that we can determine the changelog entries from the previous release
-                $StageReleaseSteps[0].with.Add('fetch-depth', 0)
-                $StageReleaseSteps += [ordered]@{
-                    name  = 'stage-release'
-                    shell = 'pwsh'
-                    run   = $StageReleaseParams + "`n" + $RunCommand
-                }
-
-                $ReleaseSteps = $DefaultSteps
-                $ReleaseSteps += [ordered]@{
-                    name  = 'build-and-release-module'
-                    shell = 'pwsh'
-                    run   = $ReleaseParams + "`n" + $RunCommand
-                }
-
-                $ReleaseJobs = @{
-                    JobTitle = 'release'
-                    RunsOn   = 'ubuntu-latest'
-                    Steps    = $ReleaseSteps
-                }
+                @($BuildsWorkflowPath, $StageReleaseWorkflowPath, $ReleaseWorkflowPath) | Assert-PathDoesNotExist
 
                 try
                 {
-                    $BuildWorkflowContent = New-GitHubActionsWorkflow `
-                        -Name 'build' `
-                        -ExecuteOn 'pull_request' `
-                        -Jobs $BuildJobs
-                    if (!$BuildWorkflowContent)
-                    {
-                        throw 'Workflow content empty.'
-                    }
-                    $StageReleaseWorkflowContent = New-GitHubActionsWorkflow `
-                        -Name 'stage-release' `
-                        -ExecuteOn 'workflow_dispatch' `
-                        -Jobs $StageReleaseJobs
-                    if (!$StageReleaseWorkflowContent)
-                    {
-                        throw 'Workflow content empty'
-                    }
-                    $ReleaseWorkflowContent = New-GitHubActionsWorkflow `
-                        -Name 'release' `
-                        -ExecuteOn 'workflow_dispatch' `
-                        -Jobs $ReleaseJobs
-                    if (!$ReleaseWorkflowContent)
-                    {
-                        throw 'Workflow content empty'
-                    }
+                    $BuildsWorkflowContent = New-BrownserveGitHubBuildsWorkflow -ModuleName $ModuleInfo.Name
+                    $StageReleaseWorkflowContent = New-BrownserveGitHubStageReleaseWorkflow -ModuleName $ModuleInfo.Name
+                    $ReleaseWorkflowContent = New-BrownserveGitHubReleaseWorkflow -ModuleName $ModuleInfo.Name
                 }
                 catch
                 {
-                    throw "Failed to create GitHub Actions workflow.`n$($_.Exception.Message)"
+                    throw "Failed to generate GitHub Actions workflow content.`n$($_.Exception.Message)"
                 }
-                Write-Verbose "Build workflow content:`n$BuildWorkflowContent"
-                Write-Verbose "Stage release workflow content:`n$StageReleaseWorkflowContent"
-                Write-Verbose "Release workflow content:`n$ReleaseWorkflowContent"
+
                 try
                 {
                     if (!(Test-Path $GitHubDirectory))
@@ -186,9 +77,9 @@ function New-BrownservePowerShellModuleBuild
                     }
                     if (!(Test-Path $WorkflowDirectory))
                     {
-                        New-Item $WorkflowDirectory -ItemType Directory -ErrorAction 'stop' | Out-Null
+                        New-Item $WorkflowDirectory -ItemType Directory -ErrorAction 'Stop' | Out-Null
                     }
-                    New-Item $BuildWorkflowPath -ItemType File -Value $BuildWorkflowContent -ErrorAction 'Stop' | Out-Null
+                    New-Item $BuildsWorkflowPath -ItemType File -Value $BuildsWorkflowContent -ErrorAction 'Stop' | Out-Null
                     New-Item $StageReleaseWorkflowPath -ItemType File -Value $StageReleaseWorkflowContent -ErrorAction 'Stop' | Out-Null
                     New-Item $ReleaseWorkflowPath -ItemType File -Value $ReleaseWorkflowContent -ErrorAction 'Stop' | Out-Null
                 }
